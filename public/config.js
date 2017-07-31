a89c778fce1b5a6155d870b853d12d93002d8f65
alexbnk <alexbnk@gmail.com>
2017-07-31 04:18:48 +0300

    app start

---
+++ public/config.js
@@ -0,0 +1,6 @@
+var config = {
+    apiKey: 'AIzaSyCHohSWJsm2ujcVPdUYmvr_FrPbhRXMkG4',
+    authDomain: 'test1-a924d.firebaseapp.com',
+    databaseURL: 'https://test1-a924d.firebaseio.com',
+    storageBucket: 'test1-a924d.appspot.com'
+};
